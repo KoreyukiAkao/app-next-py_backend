6faa6bb0f5db2372cf4d989c24bb2bf71216455e
KoreyukiAkao <koreyuki1007@gmail.com>
2025-02-24 02:27:13 +0900

    ✨WikipediaAPIと接続

--- main.py
+++ main.py
@@ -3,36 +3,60 @@
 # 通常、ブラウザはセキュリティのために異なるオリジン間のリクエストを制限します。
 # CORSを設定することで、特定のオリジンからのリクエストを許可し、データ共有を可能にします。
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import wikipediaapi
 
 # FastAPIのアプリケーションを作成
 app = FastAPI()
 
-# CORSの設定を追加
+# CORSミドルウェアの設定
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # フロントエンドのオリジンを指定
+    allow_origins=["http://localhost:3000"],  # フロントエンドのオリジンを許可
     allow_credentials=True,
     allow_methods=["*"],  # すべてのHTTPメソッドを許可
-    allow_headers=["*"],  # すべてのヘッダーを許可
+    allow_headers=["*"],  # すべてのHTTPヘッダーを許可
 )
 
 class Theme(BaseModel):
     theme: str
 
+class Query(BaseModel):
+    query: str
+
 # ルートURLにアクセスしたときの処理
 @app.get("/")
 async def read_root():
     # "Hello": "World"というメッセージを返す
-    return {"Hello!": "World!"}
+    return {"Hello": "World"}
 
 # /motivateエンドポイントにPOSTリクエストが来たときの処理
-@app.post("/motivate")
-async def motivate(theme: Theme):
-    print(f"Received theme: {theme.theme}")
-    # ここにAIサービスとの連携ロジックを追加する
-    # 例えば、AIにテーマを送ってモチベーションを高めるメッセージを生成する
-    return {"message": f"Motivation for {theme.theme}"}
-    # テーマに基づいたモチベーションメッセージを返す
+@app.post("/summarize")
+async def summarize(query: Query):
+    # print(f"Received query: {query.query}")
+    summary = get_wikipedia_summary(query.query)
+    # print(f"Received query: {summary}")
+    if summary:
+        return {"summary": summary}
+    else:
+        raise HTTPException(status_code=404, detail="記事が見つかりません")
+
+def get_wikipedia_summary(query, max_length=500):
+    # ユーザーエージェントを指定してWikipediaインスタンスを作成
+    wiki_wiki = wikipediaapi.Wikipedia(
+        language='ja',
+        user_agent='app-next-py/1.0 (https://app-next-py-lif4.vercel.app/; koreyuki1007@gmail.com)'
+    )
+    page = wiki_wiki.page(query)
+
+    if page.exists():
+        summary = page.summary
+        # 要約の長さを調整
+        if len(summary) > max_length:
+            summary = summary[:max_length] + "..."
+            print(summary)
+        return summary
+    else:
+        return "指定された記事は存在しません。"
