db5da035cd61c93d2935600d35cf2f93b86bd29b
KoreyukiAkao <koreyuki1007@gmail.com>
2025-02-25 15:49:21 +0900

    機能：ページURLの取得（readmeテスト記入）

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-# CORS（Cross-Origin Resource Sharing）について:
-# CORSは、異なるオリジン（ドメイン、プロトコル、ポート）間でのリソース共有を許可する仕組みです。
-# 通常、ブラウザはセキュリティのために異なるオリジン間のリクエストを制限します。
-# CORSを設定することで、特定のオリジンからのリクエストを許可し、データ共有を可能にします。
-
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import wikipediaapi
 from dotenv import load_dotenv
 import os
+
 
 load_dotenv()
 
@@ -46,17 +42,15 @@ async def read_root():
 
 # /motivateエンドポイントにPOSTリクエストが来たときの処理
 @app.post("/summarize")
+
 async def summarize(query: Query):
-    # print(f"Received query: {query.query}")
-    summary = get_wikipedia_summary(query.query)
-    # print(f"Received query: {summary}")
+    summary, url = get_wikipedia_summary(query.query)
     if summary:
-        return {"summary": summary}
+        return {"summary": summary, "url": url}
     else:
         raise HTTPException(status_code=404, detail="記事が見つかりません")
 
 def get_wikipedia_summary(query, max_length=500):
-    # ユーザーエージェントを指定してWikipediaインスタンスを作成
     wiki_wiki = wikipediaapi.Wikipedia(
         language='ja',
         user_agent='app-next-py/1.0 (https://app-next-py-lif4.vercel.app/; koreyuki1007@gmail.com)'
@@ -65,10 +59,9 @@ def get_wikipedia_summary(query, max_length=500):
 
     if page.exists():
         summary = page.summary
-        # 要約の長さを調整
         if len(summary) > max_length:
             summary = summary[:max_length] + "..."
-            print(summary)
-        return summary
+        return summary, page.fullurl
     else:
-        return "指定された記事は存在しません。"
+        print("Page does not exist for query:", query)  # デバッグ: ページが存在しない場合の出力
+        return "指定された記事は存在しません。", None
